0cce2af342ed670cd3eddafed28fc22d8c743e0f
Darren <darren.lieu2000@gmail.com>
2022-04-17 17:28:16 +1000

    Implemented Portfolio and Qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,10 +47,41 @@ skillsHeader.forEach((el) => {
 
 /*==================== QUALIFICATION TABS ====================*/
 
-/*==================== SERVICES MODAL ====================*/
+// activate once work experience is added
+
+// const tabs = document.querySelectorAll("[data-target]"),
+//   tabContents = document.querySelectorAll("[data-content]");
+
+// tabs.forEach((tab) => {
+//   tab.addEventListener("click", () => {
+//     const target = document.querySelector(tab.dataset.target);
+
+//     tabContents.forEach((tabContent) => {
+//       tabContent.classList.remove("qualification__active");
+//     });
+//     target.classList.add("qualification__Active");
+
+//     tab.forEach((tab) => {
+//       tab.classList.remove("qualification__active");
+//     });
+//     tab.classList.add("qualification__active");
+//   });
+// });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 /*==================== TESTIMONIAL ====================*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
